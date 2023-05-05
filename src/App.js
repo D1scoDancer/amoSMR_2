a9d3148040cc6bcfdcc3b30a7f526af629499db8
D1scoDancer <apshulikov@edu.hse.ru>
2023-05-05 17:22:12 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="main">
+      <div className="header">Header</div>
+      <div className="block">
+        <div className="left">Left</div>
+        <div className="right">Right</div>
+      </div>
+      <div className="orderBtn">OrderBtn</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
 }
 
 export default App;
